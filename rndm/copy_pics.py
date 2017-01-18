7b0ca3bfd0d197b49333075d9848ef40a278b8b1
Ilia Karmanov <ilkarman@microsoft.com>
2017-01-19 00:29:32 +0400

    ignore

---
+++ rndm/copy_pics.py
@@ -0,0 +1,20 @@
+""" Python 3 to copy all jpg files into sub-folders
+based on day-created """
+import os
+from datetime import datetime
+from shutil import copyfile
+
+# Set these two paths
+IMG_SRC = "C:\\Users\\ilkarman\\Pictures"
+IMG_DEST = "C:\\Users\\ilkarman\\Documents\\MyTestFolder"
+
+for f in os.listdir(IMG_SRC):
+    # .. or f.endswith('.png')
+    if f.endswith('.jpg'):
+        # date created (windows only); daily
+        fldr_name = "imgs_" + datetime.fromtimestamp(int(os.path.getctime(IMG_SRC + "\\" + f))).strftime('%Y_%m_%d')
+        dst = IMG_DEST + "\\" + fldr_name
+        # makedirs can ignore exist warning
+        os.makedirs(dst, exist_ok=True)
+        copyfile(IMG_SRC + "\\" + f, dst + "\\" + f)
+        print("Copied :", f)

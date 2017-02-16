6854d3aa7866e277bb45feb0cbebd61f623990e9
Ilia Karmanov <ilkarman@microsoft.com>
2017-02-16 10:53:47 +0000

    Flsk Server
    Rewrote flask python server and added load test

--- Flask Images/app_v2.py
+++ Flask Images/app_v2.py
@@ -1,42 +1,106 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, json
 from io import BytesIO
 from PIL import Image, ImageOps
+import json
 import base64
 import urllib.parse
-# Additions for image classification
 import mxnet as mx
 import numpy as np
 from collections import namedtuple
-import time
 
 Batch = namedtuple('Batch', ['data'])
-
 app = Flask(__name__)
+
+# Load Labels
+with open('synset.txt', 'r') as f:
+    synsets = [l.rstrip() for l in f]
+with open('synset_es.txt', 'r') as f:
+    synset_es = [l.rstrip() for l in f]
+with open('synset_tr.txt', 'r') as f:
+    synset_tr = [l.rstrip() for l in f]
+with open('synset_ru.txt', 'r') as f:
+    synset_ru = [l.rstrip() for l in f]
+with open('synset_it.txt', 'r') as f:
+    synset_it = [l.rstrip() for l in f]
+
+available_languages = {
+	"languages": [
+		{'name':'Spanish', 'icon':'es.png'},
+		{'name':'Italian', 'icon':'it.png'},
+		{'name':'Turkish', 'icon':'tr.png'},
+		{'name':'Russian', 'icon':'ru.png'}
+		]}
+
+# Load model
+model = mx.model.FeedForward.load('resnet-152', 0, ctx=mx.cpu())
  
 @app.route("/")
 def index():
     return render_template('layout.html')
 
+@app.route("/languages", methods=['GET'])
+def return_languages():
+	print(available_languages)
+	return json.dumps(available_languages)
+
+@app.route("/uploader_ios", methods=['POST'])
+def upload_file_ios():
+	# Get image from upload and stream into PIL object
+	print('Mobile Request')
+	imagefile = request.files['imagefile']
+	img = Image.open(BytesIO(imagefile.read())).convert('RGB')
+	print("Opened image")
+	print(img.size)
+
+	# Run some processing and get back image and data
+	_, ret_dta_en, ret_dta_es, ret_dta_tr, ret_dta_ru, ret_dta_it = run_some_function_using_image_as_input(img)
+	classification = {
+		'English': ret_dta_en,
+		'Spanish': ret_dta_es,
+		'Italian': ret_dta_it,
+		'Turkish': ret_dta_tr,
+		'Russian': ret_dta_ru
+	}
+
+	print("Image: %s classified as %s" % (imagefile.filename, classification))
+	classification_out = json.dumps(classification)
+
+	return classification_out
+
+def run_some_function_using_image_as_input(img):
+
+	# Load image
+	img = ImageOps.fit(img, (224, 224), Image.ANTIALIAS)
+	
+	# Change shape
+	img_np = np.swapaxes(img, 0, 2)
+	img_np = np.swapaxes(img_np, 1, 2) 
+	img_np = img_np[np.newaxis, :] 
+
+	# Prediction
+	res = model.predict(mx.nd.array(img_np), mx.cpu())
+	prob = np.squeeze(res)
+
+	# Category
+	a = np.argsort(prob)[-1]    
+	classification = " ".join(synsets[a].split(" ")[1:]).split(",")[0]
+
+	return img, classification, synset_es[a], synset_tr[a], synset_ru[a], synset_it[a]
+
+#############
+# OLD Follows
+#############
+
+# For web-interface ... temporary
 @app.route("/uploader", methods=['POST'])
 def upload_file():
 	# Get image from upload and stream into PIL object
 	imagefile = request.files['imagefile']
-
 	try:
-
 		img = Image.open(BytesIO(imagefile.read())).convert('RGB')
 
-		# Remove the alpha channel (want R, G, B)
-		#print("Image channels: ", len(img.split()))
-		#print("Image mode: ", img.mode)
-		#if imagefile.filename.split('.')[-1] == "png":
-		#	img.load()
-		#	img_no_alpha = Image.new("RGB", img.size, (255, 255, 255))
-		#	img_no_alpha.paste(img, mask=img.split()[3])
-		#	img = img_no_alpha
-
 		# Run some processing and get back image and data
-		ret_img, ret_dta = run_some_function_using_image_as_input(img)
+		ret_img, ret_dta, ret_dta_es, ret_dta_tr, ret_dta_ru, ret_dta_it = run_some_function_using_image_as_input(img)
 
 		# Return image and data
 		ret_imgio = BytesIO()
@@ -63,40 +127,26 @@ def upload_file():
 
 	return render_template('layout.html', **locals())
 
-
-def run_some_function_using_image_as_input(img):
-
-	# Load image
-	img = ImageOps.fit(img, (224, 224), Image.ANTIALIAS)
-	
-	# Change shape
-	img_np = np.swapaxes(img, 0, 2)
-	img_np = np.swapaxes(img_np, 1, 2) 
-	img_np = img_np[np.newaxis, :] 
-
- 	# Load model and labels
-	with open('synset.txt', 'r') as f:
-	    synsets = [l.rstrip() for l in f]
-
-	sym, arg_params, aux_params = mx.model.load_checkpoint('resnet-152', 0)
-
-	# Create model
-	mod = mx.mod.Module(symbol=sym, context=mx.cpu())
-	mod.bind(for_training=False, data_shapes=[('data', (1,3,224,224))])
-	mod.set_params(arg_params, aux_params)
-  
-	# Prediction
-	mod.forward(Batch([mx.nd.array(img_np)]))
-	prob = mod.get_outputs()[0].asnumpy()
-	prob = np.squeeze(prob)
-	
-	#time.sleep(10)
-
-	# Category
-	a = np.argsort(prob)[-1]    
-	classification = " ".join(synsets[a].split(" ")[1:]).split(",")[0]
-
-	return img, classification
+#def ms_translate_call(toLang, textTranslate, fromLang="en"):
+#
+#	""" API call to MS Cognitive Services, Translation tool """
+#    
+#    # Get token
+#    acc_key = "SECRET"
+#    headers = {"Ocp-Apim-Subscription-Key": acc_key}
+#    tok_url = "https://api.cognitive.microsoft.com/sts/v1.0/issueToken"
+#    
+#    r_tok = requests.post(tok_url, headers=headers).text  
+#    
+#    # Request for translation
+#    headers = {"Authorization ": "Bearer " + r_tok}
+#    translateUrl = "http://api.microsofttranslator.com/v2/Http.svc/Translate?text={}&from={}&to={}".format(
+#        textTranslate, fromLang, toLang)
+#
+#    r_trans = requests.get(translateUrl, headers=headers)
+#    rsp = r_trans.text
+#    soup = BeautifulSoup(rsp, "lxml")
+#    return soup.string
 
 
 if __name__ == "__main__":
